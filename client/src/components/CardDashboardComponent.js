a5bac2137dbe85aa0ae1c0b1163732728788168a
DouglasANS <douglasans@hotmail.com>
2021-07-21 17:08:57 -0300

    Ajeitando os layouts 2

--- client/src/components/CardDashboardComponent.js
+++ client/src/components/CardDashboardComponent.js
@@ -1,12 +1,21 @@
 import styles from '../styles/CardDashboardComponent.module.css'
 
+const asd= require('../')
+
 export default function CardDashboardComponent(props){
   
     return(  
         <div className={styles.container}>
+        <div className={styles.five}>
             <h1>{props.title}</h1>
             <h1>{props.number}</h1>
         </div>
+        <div className={styles.five}>
+            <img src="/todos.png" /> 
+        </div>
+        
+       
+    </div>
     )
 }
 
@@ -14,8 +23,15 @@ export function Card2(props){
   
     return(  
         <div className={styles.container}>
-            <h1>{props.title}</h1>
-            <h1>{props.number}</h1>
+            <div className={styles.five}>
+                <h1>{props.title}</h1>
+                <h1>{props.number}</h1>
+            </div>
+            <div className={styles.five}>
+                <img src="/medicoicon.png" /> 
+            </div>
+            
+           
         </div>
     )
 }
@@ -24,9 +40,84 @@ export function Card3(props){
   
     return(  
         <div className={styles.container}>
-            <h1>{props.title}</h1>
-            <h1>{props.number}</h1>
+            <div className={styles.five}>
+                <h1>{props.title}</h1>
+                <h1>{props.number}</h1>
+            </div>
+            <div className={styles.five}>
+                <img src="/enfermeiroicon.png" /> 
+            </div>
             
+           
         </div>
     )
 }
+
+export function Card4(props){
+  
+    return(  
+        <div className={styles.container}>
+            <div className={styles.five}>
+                <h1>{props.title}</h1>
+                <h1>{props.number}</h1>
+            </div>
+            <div className={styles.five}>
+                <img src="/tecenfermagemicon.png" /> 
+            </div>
+            
+           
+        </div>
+    )
+}
+
+export function Card5(props){
+  
+    return(  
+        <div className={styles.container}>
+            <div className={styles.five}>
+                <h1>{props.title}</h1>
+                <h1>{props.number}</h1>
+            </div>
+            <div className={styles.five}>
+                <img src="/fonoicon.png" /> 
+            </div>
+            
+           
+        </div>
+    )
+}
+
+export function Card6(props){
+  
+    return(  
+        <div className={styles.container}>
+            <div className={styles.five}>
+                <h1>{props.title}</h1>
+                <h1>{props.number}</h1>
+            </div>
+            <div className={styles.five}>
+                <img src="/nutricionistaicon.png" /> 
+            </div>
+            
+           
+        </div>
+    )
+}
+
+export function Card7(props){
+  
+    return(  
+        <div className={styles.container}>
+            <div className={styles.five}>
+                <h1>{props.title}</h1>
+                <h1>{props.number}</h1>
+            </div>
+            <div className={styles.five}>
+                <img src="/fisioterapiaicon.png" /> 
+            </div>
+            
+           
+        </div>
+    )
+}
+
--- client/src/components/DashboardComponent.js
+++ client/src/components/DashboardComponent.js
@@ -2,7 +2,7 @@ import React, { useContext, useEffect, useState } from 'react'
 import {CountRequest} from '../Request/CountGetRequest'
 import styles from '../styles/DashboardComponent.module.css'
 import { AuthContext } from '../context/AuthContext';
-import CardDashboardComponent, { Card2 } from '../components/CardDashboardComponent';
+import CardDashboardComponent, { Card2, Card3, Card4, Card5, Card6, Card7 } from '../components/CardDashboardComponent';
 
 export default function DashBoardComponent(){
     const [all, setAll] = useState([])
@@ -25,18 +25,19 @@ export default function DashBoardComponent(){
 
     
     return(  
-        <div >
-            <div className={styles.container}>
-            <CardDashboardComponent title="Todos" number={all}  />
-            <Card2  title="Medicina" number={medico} />
-            <CardDashboardComponent title="Enfermagem" number={enfermeiro} />
-            <CardDashboardComponent title="Técnico em enfermagem" number={tecnicoEnfermagem} />
+        <div className={styles.fundo}>
+             <div className={styles.container}>
+            <CardDashboardComponent title="Total" number={all}  />
             </div>
             <div className={styles.container}>
-                
-            <CardDashboardComponent title="Fonoaudiologia" number={fonoaudiologia} />
-            <CardDashboardComponent title="Nutrição" number={nutricionista} />
-            <CardDashboardComponent title="Fisioterapia" number={fisioterapeuta} />
+            <Card2  title="Medicina" number={medico} />
+            <Card3  title="Enfermagem" number={enfermeiro} />
+            <Card4  title="Técnico em enfermagem" number={tecnicoEnfermagem} />
+            </div>
+            <div className={styles.container}>
+            <Card5  title="Fonoaudiologia" number={fonoaudiologia} />
+            <Card6  title="Nutrição" number={nutricionista} />
+            <Card7  title="Fisioterapia" number={fisioterapeuta} />
             </div>
      
      
